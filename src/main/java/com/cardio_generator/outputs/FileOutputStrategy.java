957082f797608adf97c9bcc129973c9ab8c62378
Link <linksediq@gmail.com>
2024-05-16 11:49:57 +0200

    Google Java Style Guide corrections

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -8,34 +8,32 @@ import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FileOutputStrategy implements OutputStrategy {
+    private String baseDirectory; // Replaced all instances of "BaseDirectory" var with "baseDirectory"
 
-    private String BaseDirectory;
+    public final ConcurrentHashMap<String, String> fileMap = new ConcurrentHashMap<>(); // Replaced all instances of "file_map" with "fileMap"
 
-    public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
-
-    public FileOutputStrategy(String baseDirectory) {
-
-        this.BaseDirectory = baseDirectory;
+    public FileOutputStrategy(String baseDirectory) { //Replaced all instances of "fileOutputStrategy" with "FileOutputStrategy"
+        this.baseDirectory = baseDirectory; // Removed blank line above this line
     }
 
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         try {
             // Create the directory
-            Files.createDirectories(Paths.get(BaseDirectory));
+            Files.createDirectories(Paths.get(baseDirectory));
         } catch (IOException e) {
             System.err.println("Error creating base directory: " + e.getMessage());
             return;
         }
-        // Set the FilePath variable
-        String FilePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
+        // Set the filePath variable
+        String filePath = fileMap.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString()); // Replaced all instances of "FilePath" with "filePath"
 
         // Write the data to the file
         try (PrintWriter out = new PrintWriter(
-                Files.newBufferedWriter(Paths.get(FilePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+            Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) { // Removed extra indent
             out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
         } catch (Exception e) {
-            System.err.println("Error writing to file " + FilePath + ": " + e.getMessage());
+            System.err.println("Error writing to file " + filePath + ": " + e.getMessage());
         }
     }
 }
