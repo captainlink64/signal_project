fec11abd678b1c3f2997521392b4c2e983bd3624
Anastazja Ziarko <anastazja.ziarko@gmail.com>
2024-06-09 21:56:44 +0200

    datareader interface prototype

--- src/main/java/com/data_management/DataReader.java
+++ src/main/java/com/data_management/DataReader.java
@@ -1,13 +1,9 @@
 package com.data_management;
 
 import java.io.IOException;
+import java.io.Reader;
 
 public interface DataReader {
-    /**
-     * Reads data from a specified source and stores it in the data storage.
-     * 
-     * @param dataStorage the storage where data will be stored
-     * @throws IOException if there is an error reading the data
-     */
-    void readData(DataStorage dataStorage) throws IOException;
+    void readData(DataStorage dataStorage, String directoryPath) throws IOException;
+    int parseData(Reader reader, DataStorage dataStorage, int lineNumber) throws IOException;
 }
---
+++ src/main/java/com/data_management/FileDataReader.java
@@ -0,0 +1,48 @@
+package com.data_management;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.*;
+import java.util.Arrays;
+
+public class FileDataReader implements DataReader {
+
+    @Override
+    public void readData(DataStorage dataStorage, String directoryPath) throws IOException {
+        Files.walk(Paths.get(directoryPath))
+                .filter(Files::isRegularFile)
+                .forEach(filePath -> {
+                    try (Reader reader = new FileReader(filePath.toFile())) {
+                        parseData(reader, dataStorage, 0);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    @Override
+    public int parseData(Reader reader, DataStorage dataStorage, int lineNumber) throws IOException {
+        int charVal;
+        StringBuilder dataString = new StringBuilder();
+        while ((charVal = reader.read()) != -1) {
+            dataString.append((char) charVal);
+        }
+
+        String[] lines = dataString.toString().split("\n");
+        lines = Arrays.copyOfRange(lines, lineNumber, lines.length);
+
+        for (String line : lines) {
+            String[] parts = line.split(",");
+            if (parts.length != 4) {
+                throw new IOException("Invalid data format: too many properties");
+            }
+            int patientId = Integer.parseInt(parts[0]);
+            long timestamp = Long.parseLong(parts[1]);
+            String label = parts[2];
+            double data = Double.parseDouble(parts[3].replace("%", ""));
+            dataStorage.addPatientData(patientId, data, label, timestamp);
+        }
+        return lines.length;
+    }
+}
